a4fddc5df0204b649a612827a8472c4e9a4421c3
Stephen Quoll <stephenquoll@gmail.com>
2024-09-04 23:44:24 +1000

    Removing conditionals while we're concentrating on containers

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,15 +3,16 @@
 {
 
   fileSystems."/" = lib.mkDefault
-    { device = "/dev/disk/by-label/DOGEBOX";
+    { 
+      device = "/dev/disk/by-label/DOGEBOX";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" = if pkgs.system == "x86_64-linux" then 
-    lib.mkDefault {
-    device = "/dev/sda1";
-    fsType = "vfat";
-  } else { };
+  fileSystems."/boot" = lib.mkDefault 
+    {
+      device = "/dev/sda1";
+      fsType = "vfat";
+    };
 
   networking.useDHCP = lib.mkDefault true;
 
