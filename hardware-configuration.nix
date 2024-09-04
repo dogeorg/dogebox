1ac2ba51d8d3e8f0504737aec0d475dbf104bef0
Stephen Quoll <stephenquoll@gmail.com>
2024-09-04 16:57:33 +1000

    Moving mount points to defaults

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -2,12 +2,13 @@
 
 {
 
-  fileSystems."/" =
+  fileSystems."/" = lib.mkDefault
     { device = "/dev/disk/by-label/DOGEBOX";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" = if pkgs.system == "x86_64-linux" then {
+  fileSystems."/boot" = if pkgs.system == "x86_64-linux" then 
+    lib.mkDefault {
     device = "/dev/sda1";
     fsType = "vfat";
   } else { };
