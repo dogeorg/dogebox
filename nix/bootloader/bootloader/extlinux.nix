62a3c1c486bcbf05ab59b9b5ffafa2e8d6084cd3
Adam Brady <adam@boxxen.org>
2024-09-06 15:09:53 +1000

    move everything into a nix folder

---
+++ nix/bootloader/bootloader/extlinux.nix
@@ -0,0 +1,8 @@
+{ config, libs, pkgs, ... }:
+
+{
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+}
