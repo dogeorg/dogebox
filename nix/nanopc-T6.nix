3f2d437aed31d189378214f45588bef8f3b3c665
Stephen Quoll <stephenquoll@gmail.com>
2024-09-19 11:25:42 +1000

    Current WIP

--- nix/nanopc-T6.nix
+++ nix/nanopc-T6.nix
@@ -10,33 +10,48 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-#  boot.kernelPackages = let
-#    linux_rk3588_pkg = { fetchFromGitHub, buildLinux, ... } @ args:
-#
-#      buildLinux(args // rec {
-#        modDirVersion = "6.1.57";
-#        version = modDirVersion;
-#
-#        src = fetchFromGitHub {
-#          owner = "friendlyarm";
-#          repo = "kernel-rockchip";
-#          rev = "85d0764ec61ebfab6b0d9f6c65f2290068a46fa1";
-#          hash = "sha256-oGMx0EYfPQb8XxzObs8CXgXS/Q9pE1O5/fP7/ehRUDA=";
-#        };
-#        kernelPatches = [];
-#
-#        extraConfig = "";
-#
-#        extraMeta.branch = "6.1";
-#
-#        configFile = ./nanopc-T6_linux_defconfig;
-#
-#      } // (args.Override or {}));
-#    linux_rk3588 = pkgs.callPackage linux_rk3588_pkg{};
-#  in
-#    pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rk3588);
+  boot.kernelPackages = let
+    linux_rk3588_pkg = {
+                        fetchFromGitHub,
+                        linuxManualConfig,
+                        ubootTools,
+                         ...
+                      } :
+    (linuxManualConfig rec {
+      modDirVersion = "6.1.43";
+      version = "${modDirVersion}-xunlong-rk3588";
+      extraMeta.branch = "6.1";
 
-  boot.initrd.availableKernelModules = [ "nvme" "usbhid" ];
+      # https://github.com/orangepi-xunlong/linux-orangepi/tree/orange-pi-6.1-rk35xx
+      src = fetchFromGitHub {
+        owner = "orangepi-xunlong";
+        repo = "linux-orangepi";
+        rev = "752c0d0a12fdce201da45852287b48382caa8c0f";
+        hash = "sha256-tVu/3SF/+s+Z6ytKvuY+ZwqsXUlm40yOZ/O5kfNfUYc=";
+      };
+
+      configfile = ./nanopc-T6_linux_defconfig;
+
+      allowImportFromDerivation = true;
+
+#  modulesClosure = pkgs.makeModulesClosure {
+#    rootModules = config.boot.initrd.availableKernelModules ++ config.boot.initrd.kernelModules;
+#    kernel = modulesTree;
+#    firmware = firmware;
+#    allowMissing = false;
+#  };
+
+    })
+    .overrideAttrs (old: {
+      name = "k"; # dodge uboot length limits
+      nativeBuildInputs = old.nativeBuildInputs ++ [ubootTools];
+    });
+      linux_rk3588 = pkgs.callPackage linux_rk3588_pkg{};
+    in
+      pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rk3588);
+
+  #boot.initrd.availableKernelModules = [ "nvme" "usbhid" ];
+  boot.initrd.availableKernelModules = [];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -50,7 +65,13 @@
     cloud-utils
     parted
     wpa_supplicant
+    screen
   ];
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = true;
+  };
 
   systemd.services.resizerootfs = {
     description = "Expands root filesystem on first boot";
