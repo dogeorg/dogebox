951a648dd55ba50b11c2c8ac99b9b754520aecfc
Adam Brady <adam@boxxen.org>
2024-10-10 13:41:23 +1100

    override raw image name

--- nix/builders/nanopc-t6/builder.nix
+++ nix/builders/nanopc-t6/builder.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, ... }:
+{ pkgs ? import <nixpkgs> {}, modulesPath, lib, config, ... }:
 
 let
   dogebox = import <dogebox> { inherit pkgs; };
@@ -33,15 +33,36 @@ let
     text = builtins.readFile ../../dbx/dkm.nix;
   };
 
- firmwareFile = pkgs.writeTextFile {
-   name = "firmware.nix";
-   text = builtins.readFile ./firmware.nix;
- };
+  firmwareFile = pkgs.writeTextFile {
+    name = "firmware.nix";
+    text = builtins.readFile ./firmware.nix;
+  };
+
+  imageName = "dogebox-t6";
+
+  # Override make-disk-image to pass in values that aren't properly exposed via nixos-generators
+  # https://github.com/nix-community/nixos-generators/blob/master/formats/raw.nix#L23-L27
+  baseRawImage = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    diskSize = "auto";
+    format = "raw";
+    name = imageName;
+  };
 in
 {
   imports = [
     ./base.nix
   ];
+
+  system.build.raw = lib.mkForce (pkgs.stdenv.mkDerivation {
+    name = "dogebox-t6.img";
+    buildInputs = [ baseRawImage ];
+    buildCommand = ''
+      mkdir -p $out
+      cp -R ${baseRawImage}/* $out/
+      mv $out/nixos.img $out/${imageName}.img
+    '';
+  });
 
   system.activationScripts.copyFiles = ''
     mkdir -p /opt
