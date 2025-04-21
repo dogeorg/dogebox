3740e08f28c1fca05530179b670783df4fd76019
Adam Brady <adam@boxxen.org>
2025-04-21 22:45:50 +1000

    nix dev shell

--- flake.nix
+++ flake.nix
@@ -1,47 +1,58 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    dogeboxd = {
+      url = "github:dogebox-wg/dogeboxd/dev-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dkm = {
+      url = "github:dogebox-wg/dkm/dev-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, nixos-generators,... } @ inputs: let
-    # Both of these MUST be updated to successfully build a new
-    # release, otherwise nix will silently cache things.
+  outputs = { self, nixpkgs, nixos-generators, flake-utils, ... } @ inputs: let
     dbxRelease = "v0.3.2-beta.3";
-    nurPackagesHash = "4e3a0c35aef994e3ec5516ccbb7c202a0cb8fb27";
-
-    developmentMode = builtins.getEnv "dev" == "1";
-
-    devConfig = if developmentMode then
-      builtins.fromJSON (builtins.readFile ./dev.json)
-    else
-      {};
-
-    localDogeboxdPath = if (builtins.hasAttr "dogeboxd" devConfig) then devConfig.dogeboxd else null;
-    localDpanelPath = if (builtins.hasAttr "dpanel" devConfig) then devConfig.dpanel else null;
-    dogeboxNurPackagesPath = if (builtins.hasAttr "nur" devConfig) then devConfig.nur else builtins.fetchGit {
-      url = "https://github.com/dogeorg/dogebox-nur-packages.git";
-      ref = "refs/tags/${dbxRelease}";
-      rev = nurPackagesHash;
-    };
-
-    dbx = system: import (dogeboxNurPackagesPath + "/default.nix") {
-      pkgs = import nixpkgs {
-        system = system + "-linux";
-      };
-      inherit localDogeboxdPath localDpanelPath dbxRelease nurPackagesHash;
-    };
 
     base = arch: builder: format: nixos-generators.nixosGenerate {
       system = arch + "-linux";
-      modules = [ builder ];
+      modules = [
+        builder
+        {
+          nix.registry.nixpkgs.flake = nixpkgs;
+        }
+      ];
       format = format;
       specialArgs = {
-        inherit arch dbxRelease nurPackagesHash;
-        dogebox = dbx arch;
+        inherit arch dbxRelease;
       };
+    };
+
+    mkDevShell = system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in pkgs.mkShell {
+      buildInputs = [
+        pkgs.gnumake
+        pkgs.nixos-generators
+        pkgs.swig
+        pkgs.git
+        pkgs.parted
+        pkgs.btrfs-progs
+        pkgs.e2fsprogs
+        pkgs.rsync
+        pkgs.wget
+        pkgs.simg2img
+        pkgs.exfat
+        pkgs.exfatprogs
+      ];
     };
   in {
     t6 = base "aarch64" ./nix/builders/nanopc-t6/builder.nix "raw";
@@ -54,5 +65,10 @@
 
     qemu-x86_64 = base "x86_64" ./nix/builders/qemu/builder.nix "qcow";
     qemu-aarch64 = base "aarch64" ./nix/builders/qemu/builder.nix "qcow";
+
+    devShell = {
+      "x86_64-linux" = mkDevShell "x86_64-linux";
+      "aarch64-linux" = mkDevShell "aarch64-linux";
+    };
   };
 }
